0d4d98d8f7c27c345ee99c5f1ac238bad759186f
Samuel Kim <samkunkim@gmail.com>
2024-02-28 14:45:23 -0800

    Initial Commit

---
+++ functions.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+double calculateTip (double bill, int percent) {
+  // TODO: Take in the bill total using a double, and a percentage as an int
+  // Calculate the total
+}
+
+void printReceipt (double subtotal, double tip) {
+  // TODO: Get the terminal to print out the following:
+  // Your sub-total: (Total bill before tip here)
+  // Your tip: (Tip in dollars and cents)
+  // Total bill: (Add them together)
+}
